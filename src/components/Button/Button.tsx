3c168f18779275ef6354afdde91e4cba06d7d7de
sergejij <ushakoff.sergej2010@yandex.ru>
2022-05-31 22:19:58 +0300

    correct lib

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import styles from './Button.module.css';
 
+type SizeType = "S" | "M" | "L";
+
 export interface ButtonProps {
     label: string;
+    size: SizeType;
 }
 const Button = (props: ButtonProps) => {
     return <button className={styles.button}>{props.label}</button>;
