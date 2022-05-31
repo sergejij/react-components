5aaf3bc1297f8ef1100780976ef008522a22ffd0
sergejij <ushakoff.sergej2010@yandex.ru>
2022-05-31 19:38:12 +0300

    made lib

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './Button.module.css';
+
+export interface ButtonProps {
+    label: string;
+}
+const Button = (props: ButtonProps) => {
+    return <button className={styles.button}>{props.label}</button>;
+};
+export default Button;
